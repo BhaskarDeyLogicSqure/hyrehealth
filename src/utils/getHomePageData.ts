50c57e7edbbf7591e475e0086aab1c037c19a784
sauvik <sauvik@logic-square.com>
2026-01-12 12:25:47 +0530

    feat: enhance API calls in home page to include origin header and FDA approval filtering for featured products

--- src/utils/getHomePageData.ts
+++ src/utils/getHomePageData.ts
@@ -1,6 +1,7 @@
 import { categoryApi } from "@/api/categories/categoryApi";
 import { productApi } from "@/api/products/productApi";
 import { profileApi } from "@/api/profile/profileApi";
+import { authApi } from "@/api/auth/authApi";
 import { getCurrentDomain } from "@/lib/utils";
 import { headers } from "next/headers";
 
@@ -10,10 +11,18 @@ export const getHomePageData = async () => {
     const headersList = headers();
     const origin = getCurrentDomain(headersList);
 
+    // Fetch merchant data server-side
+    const merchantResponse = await authApi.getMerchantNMITokenizationKey(
+      origin
+    );
+    const merchantData = merchantResponse?.data;
+
+    const fdaApproved = merchantData?.isApplyLegitScript || undefined; // undefined means both fda approved and non fda approved products will be shown
+
     const [categoryResponse, productResponse, testimonialResponse] =
       await Promise.all([
         categoryApi.getFeaturedCategories(origin),
-        productApi.getFeaturedProducts(origin),
+        productApi.getFeaturedProducts(fdaApproved, origin),
         profileApi.getFeaturedTestimonials(origin),
       ]);
 
